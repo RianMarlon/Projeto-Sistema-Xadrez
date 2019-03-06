ae7d28e162fcb2c03f2721daffdcbb46b1d81651
Rian Marlon <rianmarlon20@yahoo.com>
2019-03-06 10:16:37 -0300

    Implementando movimentos possíveis do Rei

--- src/pecasDeXadrez/King.java
+++ src/pecasDeXadrez/King.java
@@ -3,25 +3,90 @@
 package pecasDeXadrez;
 
 import jogoDeTabuleiro.Board;
+import jogoDeTabuleiro.Position;
 import xadrez.ChessPiece;
 import xadrez.Color;
 
 public class King extends ChessPiece {
 
-	//informando quem é o tabuleiro e a cor da peça
+	// informando quem é o tabuleiro e a cor da peça
 	public King(Board board, Color color) {
 		super(board, color);
 	}
 
-	//Retornar letra referente a peça 
+	// Retornar letra referente a peça
 	@Override
 	public String toString() {
 		return "R";
 	}
 
+	// Pode mover
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// Acima
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+
+		}
+
+		// Baixo
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+
+		}
+
+		// Esquerda
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+
+		}
+
+		// Direita
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+
+		}
+
+		// Noroeste
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+
+		}
+
+		// Nordeste
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+
+		}
+
+		// Sudoeste
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+
+		}
+		// Sudeste
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+
+		}
+
 		return mat;
 	}
 
